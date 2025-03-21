996080d0b311ddb99dd2a203ea0d8f2307af0a84
Usuario <Jorge>
2025-03-21 13:15:52 +0100

    Segundo commit

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ print(f"Coches de {datos["nombre"]}:")
 for coche in datos["coches"]:
     print(f"{coche}")
 #Tiene 2 teléfonos
+print(f"Tiene {len(datos["telefonos"])} telefonos")
 #Mengano no pertenece a nigún equipo
+if(datos["equipo"]) == None:
+    print(f"{datos["nombre"]} no pertenece a ningún equipo")
 #Tiene 3 hermanos reales
+print(f"Tiene {len(datos["hermanos"])} hermanos reales")
 #Mengano a veces utiliza iPhone
+print(f"{datos["nombre"]} a veces utiliza {datos["telefonos"][1]["modelo"]}")
 #Mengano tiene posiblemente 2 hermanos
+print(f"{datos["nombre"]} tiene posiblemente {len(datos["hermanos"])} hermanos")
